769610623e5a963ed8c62881943b4ae640366bec
20051682 <20051682@mydbs.ie>
2025-02-27 15:51:41 +0000

    new commit

--- terraform-test-project-111/KeyPair/main.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_key_pair" "my_key" {
-  key_name   = "Terraform-Test-Project-key-pair-111"
-  public_key = file("/Users/hiroshkoshila/terraform-test-project-111/KeyPair/Terraform-Test-Project-key-pair-111.pem") 
-}
--- terraform-test-project-111/terraform.tfvars
+++
@@ -1,2 +0,0 @@
-vpc_id           = "vpc-0de2ef56e0aac4a0c"
-public_subnet_id = "subnet-0fab5badc4424d762"
